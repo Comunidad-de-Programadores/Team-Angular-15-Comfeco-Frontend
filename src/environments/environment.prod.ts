fc8ad4e61468269eeeb81bc997929eaf82188319
Ivan Zaldivar <ivanzaldivar16@gmail.com>
2021-03-20 20:24:39 -0600

    Updated component

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  url: "https://team-angular-15-comfeco.herokuapp.com/api/v1",
+  GOOGLE_CLIENT_ID: "495092721571-pl56bohv6e5853s923d5k69d6heuqtbp.apps.googleusercontent.com",
+  FACEBOOK_CLIENT_ID: "1144377869337240"
 };
